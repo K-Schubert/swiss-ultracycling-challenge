1f65e0fa8b382d6ecd34efd6003b458a3eed4c90
kieran.schubert <kieran.schubert@hesge.ch>
2022-06-29 10:49:32 +0200

    Add julia scripts

---
+++ scripts/gtsp_solver.jl
@@ -0,0 +1,2 @@
+import GLNS
+GLNS.solver("/Users/kieranschubert/Desktop/such_optim/GTSP_FILES/test_2022.gtsp", output="/Users/kieranschubert/Desktop/such_optim/TOUR_FILES/tour_test_2022.txt")
---
+++ scripts/gtsp_solver_2022_no_transport.jl
@@ -0,0 +1,2 @@
+import GLNS
+GLNS.solver("/Users/kieranschubert/Desktop/such_optim/GTSP_FILES/test_2022_no_transport.gtsp", output="/Users/kieranschubert/Desktop/such_optim/TOUR_FILES/tour_test_2022_no_transport.txt")
