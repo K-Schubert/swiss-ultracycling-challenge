0d23aca3de413b92631b0f4ab512877258f8dc84
kieran.schubert <kieran.schubert@hesge.ch>
2022-07-04 11:06:03 +0200

    update solver

--- scripts/gtsp_solver.jl
+++ scripts/gtsp_solver.jl
@@ -1,2 +1,2 @@
 import GLNS
-GLNS.solver("/Users/kieranschubert/Desktop/such_optim/GTSP_FILES/test_2022.gtsp", output="/Users/kieranschubert/Desktop/such_optim/TOUR_FILES/tour_test_2022.txt")
+GLNS.solver("/Users/kieranschubert/Desktop/swiss-ultracycling-challenge/data/GTSP_FILES/test_2022.gtsp", output="/Users/kieranschubert/Desktop/swiss-ultracycling-challenge/data/TOUR_FILES/tour_test_2022.txt")
